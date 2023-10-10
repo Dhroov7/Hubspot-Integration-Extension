24909cee4204d5d1dc66d5b302a1ca238259fa5f
Dhroov Gupta <dhroovgupta@Dhroovs-MacBook-Air.local>
2023-10-11 01:11:15 +0530

    Integrate API to get bot response, get unique identifier for each user

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 chrome.commands.onCommand.addListener(async function (command) {
   if (command === "insert_text") {
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    await chrome.tabs.sendMessage(tab.id, '');
+    await chrome.tabs.sendMessage(tab.id, {url: tab.url});
+  }
+});
+
+chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
+  if (request.action === "getPortalId") {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    await chrome.tabs.sendMessage(tab.id, {url: tab.url});
   }
 });
--- content.js
+++ content.js
@@ -7,19 +7,48 @@ setTimeout(() => {
   if (targetElement) {
     targetElement.insertBefore(customButton, targetElement.childNodes[6]);
   }
-  customButton.addEventListener("click", function () {
-    addTextIntoBox();
+  customButton.addEventListener("click", async function () {
+    await chrome.runtime.sendMessage({ action: "getPortalId" });
   });
 }, 2000);
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  const portalId = message?.url?.split('/')[4];
+  //API call to get the active subscription using this portalID
   addTextIntoBox();
 });
 
-function addTextIntoBox() {
+async function addTextIntoBox() {
   const textBox = document.querySelectorAll(".ProseMirror")[0];
+  const result = await getBotResponse();
   if (textBox) {
     const p = textBox.getElementsByTagName("p")[0];
-    p.innerHTML = "Your desired text here";
+    p.innerHTML = result?.body;
   }
 }
+
+const getBotResponse = async () => {
+  try {
+    const url =
+      "https://theogbrand-testing--serverless-message-sender-fastapi-app.modal.run/reply/fbmessenger";
+
+    const data = {
+      customerPsid: "123456",
+      merchantPsid: "100451723148691",
+      chatMessage: "Hello",
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, requestOptions);
+    return response.json();
+  } catch (err) {
+    return {};
+  }
+};
